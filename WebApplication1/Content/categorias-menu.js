3d49f94542456398d655a2a35235f0cda719b213
balbii <renan.balbino56@gmail.com>
2023-07-23 22:36:58 -0300

    Inseri página de login e banco de dados criado

---
+++ WebApplication1/Content/categorias-menu.js
@@ -0,0 +1,8 @@
+function abrir(){
+  document.getElementById('migalha').style.marginTop = '36.5em';
+   document.getElementById('cat-escondido').style.visibility = 'visible';
+}
+function fechar(){
+  document.getElementById('migalha').style.marginTop = '25px';
+  document.getElementById('cat-escondido').style.visibility = 'hidden';
+}
---
+++ WebApplication1/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string User { get; set; }
+        public string Senha { get; set; }
+        public int ClienteId { get; set; }
+        public int CPF { get; set; }
+        public int DataNascimento { get; set; }
+    }
+}
