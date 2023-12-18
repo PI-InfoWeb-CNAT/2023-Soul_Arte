cb782224e09f1aed0c22398e45d71d9e29f5cc83
Renan Balbino de Medeiros <renan.medeiros@academico.ifrn.edu.br>
2023-12-18 14:35:48 -0300

    ...

---
+++ WebApplication1/Controllers/CarrinhosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarrinhosController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Carrinhos
+        public ActionResult Index()
+        {
+            IEnumerable<ItemCarrinho> carrinho =
+
+        HttpContext.Session["carrinho"] as IEnumerable<ItemCarrinho>;
+
+            if (carrinho == null)
+            {
+                carrinho = new List<ItemCarrinho>();
+                HttpContext.Session["carrinho"] = carrinho;
+
+            }
+            return View(carrinho);
+        }
+        public ActionResult AddProduto(FormCollection collection)
+        {
+            List<ItemCarrinho> carrinho = HttpContext.Session["carrinho"] as
+            List<ItemCarrinho>;
+            var produto2 = ObterProdutoPorId(
+            Convert.ToInt32(collection.Get("ItemCarrinhoId")));
+            var qtd = Convert.ToInt32(collection.Get("Quantidade"));
+            var itemCarrinho = new ItemCarrinho()
+            {
+                Produto = produto2,
+                Quantidade = qtd,
+                ValorUnitario = 1
+            };
+            carrinho.Add(itemCarrinho);
+            HttpContext.Session["carrinho"] = carrinho;
+            return RedirectToAction("Create");
+        }
+        private Produto ObterProdutoPorId(long id)
+        {
+            var produto = context.Produto.Find(id);
+            return produto;
+        }
+    }
+}
---
+++ WebApplication1/Models/ItemCarrinho.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ItemCarrinho
+    {
+        public long? ItemCarrinhoId { get; set; }
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+        public double ValorUnitario { get; set; }
+        public double SubTotal
+        {
+            get { return Quantidade * ValorUnitario; }
+        }
+    }
+}
